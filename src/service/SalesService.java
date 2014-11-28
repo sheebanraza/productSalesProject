39cc2741bc19d19e3838524683c0d01cc64548c8
sheebanraza <shaikhsheebanz@gmail.com>
2014-11-28 12:09:12 +0530

    added javadocs

--- src/service/SalesService.java
+++ src/service/SalesService.java
@@ -11,10 +11,24 @@ import java.util.List;
  */
 
 public interface SalesService {
-
+    /**
+    *   This method adds the sales data and return persisted list of sales data.
+    *   @Param List<Sales>
+    *   @return List<Sales>  - updated copy
+    */
     public List<Sales> addSalesData(List<Sales> sales);
 
+    /**
+     *   This method adds the sales data and return persisted list of product data.
+     *   @Param List<Product>
+     *   @return List<Product>  - updated copy
+     */
     public List<Product> addProductData(List<Product> productList);
 
+    /**
+     *   This method gets the total sales data from the database.
+     *   @Param
+     *   @return List  - result list
+     */
     public List<Object[]> getSalesData();
 }
